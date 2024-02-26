bd49ad4042e2a3d85c2b6cef496292af35ef5ff0
Nikola Milicevic <nikola260896@gmail.com>
2024-02-26 08:57:31 +0000

    Add early stopping with delta

--- framework/train.py
+++ framework/train.py
@@ -62,7 +62,26 @@ class Classifier(nn.Module):
             self.classifier = nn.Sequential(*layers)
 
     def forward(self, x):
-        return self.classifier(x)
+        x = self.classifier(x)
+        return F.softmax(x)
+
+
+class EarlyStopper:
+    def __init__(self, patience=5, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float("inf")
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
 
 
 def get_all_sentences(ds, lang):
