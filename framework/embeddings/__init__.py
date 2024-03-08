5f9f32bda591b83fab7222fb665e2dbec49dd829
zhangchao162 <1623804006@qq.com>
2024-03-08 14:03:25 +0800

    convert the `base embedding` class into a standalone package.

---
+++ framework/embeddings/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 3/7/24 5:22 PM
+# @Author  : zhangchao
+# @File    : __init__.py.py
+# @Email   : zhangchao5@genomics.cn
+from .embeddings import Embeddings
---
+++ framework/embeddings/embeddings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 3/7/24 4:58 PM
+# @Author  : zhangchao
+# @File    : embeddings.py
+# @Email   : zhangchao5@genomics.cn
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class Embeddings(ABC):
+    """Interface for protein language models (pLMs)"""
+
+    @abstractmethod
+    def get_embedding(self, **kwargs) -> np.ndarray:
+        """Obtain the protein sequence embeddings."""
+
+    @abstractmethod
+    def get_embedding_dim(self, **kwargs) -> int:
+        """Obtain the protein sequence embedding dimension of pLMs."""
