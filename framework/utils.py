4d50afc3a6a71d2736ff400980fbd27f0a383c3a
zhangchao162 <1623804006@qq.com>
2024-03-08 15:59:11 +0800

    modify `EarlyStopper` class

---
+++ framework/utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 3/8/24 2:51 PM
+# @Author  : zhangchao
+# @File    : utils.py
+# @Email   : zhangchao5@genomics.cn
+import numpy as np
+
+
+class EarlyStopper:
+    def __init__(self, patience=10, min_delta=1e-6):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_loss = np.Inf
+
+    def __call__(self, loss):
+        if np.isnan(loss):
+            return True
+
+        if loss < self.min_loss:
+            self.min_loss = loss
+            self.counter = 0
+        elif loss > (self.min_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
