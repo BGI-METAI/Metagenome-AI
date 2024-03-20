d8acf988688713bbe7db51ebaeecf634737b82c6
zhangchao162 <1623804006@qq.com>
2024-03-20 11:16:53 +0800

    add focal loss function

--- framework/classifier/loss_fn.py
+++ framework/classifier/loss_fn.py
@@ -4,6 +4,7 @@
 # @Author  : zhangchao
 # @File    : loss_fn.py
 # @Email   : zhangchao5@genomics.cn
+import torch
 import torch.nn.functional as F
 
 
@@ -11,3 +12,10 @@ class ProteinLoss:
     @staticmethod
     def cross_entropy_loss(pred, target, weight=1.):
         return F.cross_entropy(pred, target.long().to(pred.device)) * weight
+
+    @staticmethod
+    def focal_loss(pred, target, weight=1., gamma=2.):
+        ce_loss = F.cross_entropy(pred, target.long().to(pred.device))
+        pt = torch.exp(-ce_loss)
+        focal_loss = (1 - pt) ** gamma * ce_loss * weight
+        return focal_loss
