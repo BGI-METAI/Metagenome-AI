33b65107a1ececdf330c1ab22a847a15a206adf0
zhangchao162 <1623804006@qq.com>
2024-03-27 12:51:23 +0800

    add fine tuning block

---
+++ framework/finetuning/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/3/27 10:52
+# @Author  : zhangchao
+# @File    : __init__.py.py
+# @Email   : zhangchao5@genomics.cn
+
+
+
---
+++ framework/finetuning/finetuning_train.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/3/27 12:48
+# @Author  : zhangchao
+# @File    : finetuning_train.py
+# @Email   : zhangchao5@genomics.cn
+
+
+
