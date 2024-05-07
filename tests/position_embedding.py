c7498a3c867db66d013ac6c49646a45925c500ab
kuisu <sukui@genomics.cn>
2024-05-07 17:56:21 +0800

    1. add test script of position embedding with bert

---
+++ tests/position_embedding.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch
+import math
+
+
+class PositionalEmbedding(nn.Module):
+
+    def __init__(self, d_model, max_len=512):
+        super().__init__()
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
+
+
+if __name__ == '__main__':
+    pe = PositionalEmbedding(d_model=1024)  # d_model: 隐藏层的维度
+
+    x = torch.randn(size=(3, 10))
+    y = pe(x)
+    print(y.shape)
