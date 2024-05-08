c80505d8ee6c947fccd575035a3fc26ef2ad889b
kuisu <sukui@genomics.cn>
2024-05-08 11:23:56 +0800

    1. add NetsurfConvModel for downstream task in Netsurfa dataset

--- proteinNER/classifier/model.py
+++ proteinNER/classifier/model.py
@@ -102,3 +102,38 @@ class ProtTransT5MaskPretrainModel(nn.Module):
 
     def forward(self, input_ids, attention_mask):
         return self.classifier(self.embedding(input_ids, attention_mask))
+
+
+class NetsurfConvModel(nn.Module):
+    def __init__(self, n_final_in=32):
+        """
+        ref: https://github.com/Eryk96/NetSurfP-3.0
+        :param n_final_in: dimension for final in layer
+        """
+        super(NetsurfConvModel, self).__init__()
+        # This is only called "elmo_feature_extractor" for historic reason
+        # CNN weights are trained on ProtT5 embeddings
+        self.elmo_feature_extractor = nn.Sequential(
+            nn.Conv2d(1024, n_final_in, kernel_size=(7, 1), padding=(3, 0)),  # 7x32
+            nn.ReLU(),
+            nn.Dropout(0.25),
+        )
+        self.dssp3_classifier = nn.Sequential(
+            nn.Conv2d(n_final_in, 3, kernel_size=(7, 1), padding=(3, 0))  # 7
+        )
+
+        self.dssp8_classifier = nn.Sequential(
+            nn.Conv2d(n_final_in, 8, kernel_size=(7, 1), padding=(3, 0))
+        )
+        self.diso_classifier = nn.Sequential(
+            nn.Conv2d(n_final_in, 2, kernel_size=(7, 1), padding=(3, 0))
+        )
+
+    def forward(self, x):
+        # IN: X = (B x L x F); OUT: (B x F x L, 1)
+        x = x.permute(0, 2, 1).unsqueeze(dim=-1)
+        x = self.elmo_feature_extractor(x)  # OUT: (B x 32 x L x 1)
+        d3_Yhat = self.dssp3_classifier(x).squeeze(dim=-1).permute(0, 2, 1)  # OUT: (B x L x 3)
+        d8_Yhat = self.dssp8_classifier(x).squeeze(dim=-1).permute(0, 2, 1)  # OUT: (B x L x 8)
+        diso_Yhat = self.diso_classifier(x).squeeze(dim=-1).permute(0, 2, 1)  # OUT: (B x L x 2)
+        return d3_Yhat, d8_Yhat, diso_Yhat
