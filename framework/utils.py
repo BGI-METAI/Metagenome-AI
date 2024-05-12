adfc152de741c2b84e9cd2e81b592a5da5f17fd1
Nikola Milicevic <nikola260896@gmail.com>
2024-05-12 19:44:19 +0000

    Add utils.py with memory debugging functions

---
+++ framework/utils.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   utils.py
+@Time    :   2024/05/12 21:36:34
+@Author  :   Nikola Milicevic 
+@Version :   1.0
+@Contact :   nikola260896@gmail.com
+@License :   (C)Copyright 2024, Nikola Milicevic
+@Desc    :   Utility functions, mainly ones used for memory consumption debugging
+"""
+import torch
+
+
+def torch_gpu_mem_info(device=None):
+    if not device:
+        device = "cuda:0"
+    alloc = torch.cuda.memory_allocated(device) / (1024 * 1024)
+    res = torch.cuda.memory_reserved(device) / (1024 * 1024)
+    max_alloc = torch.cuda.max_memory_allocated(device) / (1024 * 1024)
+    max_res = torch.cuda.max_memory_reserved(device) / (1024 * 1024)
+    print(
+        f"Allocated: {alloc} Reserved: {res} Max_alloc: {max_alloc} Max_res: {max_res}"
+    )
+
+
+def reset_cache_and_peaks():
+    torch.cuda.empty_cache()
+    torch.cuda.reset_peak_memory_stats()
+
+
+def count_model_size_mb(model):
+    return sum(p.nelement() * p.element_size() for p in model.parameters()) / (
+        1024 * 1024
+    )
+
+
+def count_parameters(model):
+    return sum(p.nelement() for p in model.parameters())
+
+
+def check_gpu_mem(device=None):
+    if not device:
+        device = "cuda:0"
+    free, total = torch.cuda.mem_get_info(device)
+    print(f"Free: {free / (1024*1024)} Total: {total / (1024*1024)}")
+
+
+def get_tensor_size_mb(t):
+    sz = t.element_size() * t.nelement()
+    sz /= 1024 * 1024
+    print(f"Size MiB: {sz}")
+
+
+def is_model_on_gpu(model):
+    print(f"Model is on GPU: {next(model.parameters()).is_cuda}")
