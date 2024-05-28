9bc6b7ac760a658749aec6210539dc133fc057db
zhangchao162 <1623804006@qq.com>
2024-05-28 18:17:29 +0800

    update the post-process strategy, add the `probability`, `crf_label` and `emission_label` fields to the predict results

--- proteinNER/classifier/trainer.py
+++ proteinNER/classifier/trainer.py
@@ -93,8 +93,8 @@ class ProteinAANERTrainer(BaseTrainer):
 
         for idx, sample in enumerate(batch_iterator):
             input_ids, attention_mask, batch_label, batch_protein_ids = sample
-            pred = model.module.inference(input_ids, attention_mask)
+            results = model.module.inference(input_ids, attention_mask)
 
             for cnt in range(self.batch_size):
-                pickle.dump({'label': batch_label[cnt], 'pred': pred[cnt]},
+                pickle.dump(results[cnt].update({'ground_label': batch_label[cnt].detach().cpu().tolist()}),
                             open(osp.join(self.result_home, f'{batch_protein_ids[cnt]}.pkl'), 'wb'))
