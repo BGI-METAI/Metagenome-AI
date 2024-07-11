a82c09d4b8f8111a5917b2db5ad835d62a824a8a
Vladimir Kovacevic <upravljac@gmail.com>
2024-07-11 15:11:41 +0000

    Experiment with batch creation in progress.

--- framework/embedding_esm3.py
+++ framework/embedding_esm3.py
@@ -14,6 +14,8 @@ from huggingface_hub import login
 from esm.models.esm3 import ESM3
 from esm.sdk.api import ESM3InferenceClient
 from esm.tokenization.sequence_tokenizer import EsmSequenceTokenizer
+from esm.pretrained import ESM3_sm_open_v0
+
 from huggingface_hub.hf_api import HfFolder
 
 from embedding_esm import EsmEmbedding
@@ -24,21 +26,30 @@ class Esm3Embedding(EsmEmbedding):
         # HfFolder.save_token(access_token)
 
         # This will download the model weights and instantiate the model on your machine.
-        model: ESM3InferenceClient = ESM3.from_pretrained("esm3_sm_open_v1").to("cuda") # or "cpu"
+        # model: ESM3InferenceClient = ESM3.from_pretrained("esm3_sm_open_v1").to("cuda") # or "cpu"
+        model = ESM3_sm_open_v0("cuda")
 
         self.model = model
-        self.alphabet = EsmSequenceTokenizer()
+        self.alphabet = EsmSequenceTokenizer()  # model.get_structure_token_encoder()  #
         self.model.output_heads = Identity()
         self.model.structure_decoder = Identity()
         self.model.function_decoder = Identity()
         self.model.structure_encoder = Identity()
         self.model.eval()
-        self.batch_converter = self.alphabet.get_batch_converter()
         self.embed_dim = model.embed_dim
         self.pooling = pooling
         # Freeze the parameters of ESM
         for param in self.model.parameters():
             param.requires_grad = False
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def get_embedding(self, batch):
+        tokens = self.alphabet.encode(batch["sequence"])
+        batch_tokens = self.model(tokens)  # TODO: Debug here!
+        batch_tokens = batch_tokens.to(self.device)
+        with torch.no_grad():
+            esm_result = self.model(batch_tokens)
+
+        return self._pooling(self.pooling, esm_result["logits"], batch_tokens)
 
     
