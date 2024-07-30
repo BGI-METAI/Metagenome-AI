c9856d3afae9a0b83c52518b2c0d318585232eca
Nikola Milicevic <nikola260896@gmail.com>
2024-07-30 09:41:35 +0000

    Add ESM3 to framework

--- framework/embeddings/__init__.py
+++ framework/embeddings/__init__.py
@@ -10,3 +10,7 @@ try:
     from embeddings.embedding_protein_vec import ProteinVecEmbedding
 except ImportError:
     print("You are missing some of the libraries for ProteinVec")
+try:
+    from embeddings.embedding_esm3 import Esm3Embedding
+except ImportError:
+    print("You are missing some of the libraries for ESM3")
