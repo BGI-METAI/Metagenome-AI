627488928faa19f206ae2bc311a8d543a3b17e51
Vladimir Kovacevic <upravljac@gmail.com>
2024-08-21 12:41:25 +0200

    Add decoder to the model.

--- src/finetune_ptrans.py
+++ src/finetune_ptrans.py
@@ -23,17 +23,17 @@ class ProteinDataset(Dataset):
         labels = inputs.input_ids.clone()
 
         curr_mask_prob = random.uniform(0, self.mask_prob)
-
+        
         # Randomly mask tokens
         rand = torch.rand(labels.shape)
         mask_arr = (rand < curr_mask_prob) * (labels != self.tokenizer.pad_token_id)
         labels[~mask_arr] = tokenizer.pad_token_id  # Only calculate loss on masked tokens
-
+        
         return inputs.input_ids.squeeze(0), labels.squeeze(0)
 
 ptrans_model_path = "/home/share/huadjyin/home/vladimir/Metagenome-AI/framework/prot_t5_xl_uniref50"
-tokenizer = T5Tokenizer.from_pretrained(ptrans_model_path, do_lower_case=False, local_files_only = True)
-model = T5EncoderModel.from_pretrained(ptrans_model_path)
+tokenizer = T5Tokenizer.from_pretrained(ptrans_model_path, do_lower_case=False, local_files_only=True, legacy=False)
+model = T5ForConditionalGeneration.from_pretrained(ptrans_model_path)
 
 # Initialize tokenizer and model
 # tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_uniref50')
@@ -56,47 +56,48 @@ for df_path,sep in zip(df_paths, ['\t', ' ']):
     df = pd.read_csv(df_path, header=None, sep=sep)
     for ind, row in df.iterrows():
         sequences.append(row[0])
-        # if ind > 100:
-        #     break
+        if ind > 500:
+            break
     break
 
 
 # Create dataset and dataloader
 dataset = ProteinDataset(sequences, tokenizer, mask_prob=0.15)
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 
 # Optimizer and Loss function
 optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
 criterion = torch.nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
 
 # Training loop
-num_epochs = 30
+num_epochs = 10
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0
     for ind, batch in enumerate(dataloader):
         inputs, labels = batch
         inputs, labels = inputs.to(device), labels.to(device)
-
+        
         optimizer.zero_grad()
-        outputs = model(input_ids=inputs)
-        logits = outputs['last_hidden_state']
-
+        outputs = model(input_ids=inputs, labels=labels)
+        logits = outputs['logits']
+        
         # Reshape logits and labels for loss computation
         logits = logits.view(-1, logits.size(-1))
         labels = labels.view(-1)
-
+        
         # Compute loss
         loss = criterion(logits, labels)
         loss.backward()
         optimizer.step()
-
+        
         total_loss += loss.item()
         print(f'Processing batch: {ind}')
 
-    avg_loss = total_loss / len(dataloader)
+    avg_loss = total_loss / len(dataloader) 
     print(f"Epoch {epoch+1}/{num_epochs}, Loss: {avg_loss:.4f}")
-
+    
     # Save the model after each epoch
     model.save_pretrained(f"ptrans_finetuned_epoch_{epoch+1}")
+    # Probably no need to save tokenizer but let's be sure
     tokenizer.save_pretrained(f"ptrans_finetuned_epoch_{epoch+1}")
